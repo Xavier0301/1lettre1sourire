51797b2e75ec5d14d2611c26af296777f2586dc2
Xavier Servot <xavier.servot@live.fr>
2020-04-04 12:46:09 +0200

    Pushing the untracked files

---
+++ app/routes/login.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var User = require('../models/user');
+
+router.post('/', function(req, res, next) {
+    if (req.body.username && req.body.password) {
+      User.authenticate(req.body.username, req.body.password, function (error, user) {
+        if (error || !user) {
+          var err = new Error('Wrong username or password.');
+          err.status = 401;
+          return next(err);
+        } else {
+          req.session.userId = user._id;
+          return res.send({ username: user.username, admin: user.isAdmin });
+        }
+      });
+    } else {
+      var err = new Error('Username and password are required.');
+      err.status = 401;
+      return next(err);
+    }
+  });
+
+module.exports = router;

163b31a042f9df2cb944131e0c6d51bd3c1446e5
BDURON99 <33147418+BDURON99@users.noreply.github.com>
2020-04-05 03:36:03 +0200

    draft for login script

---
+++ src/login/Ressources/js/login.js
@@ -0,0 +1,10 @@
+// DOCTYPE javascript>
+
+function validateLogin() {
+    if (true) {
+        window.location = "index.html";//@TODO remplasser index.html par la page de review
+        return false;
+    } else {
+        alert ("Mauvais nom d'utilisateur ou mot de passe, essaie à nouveau");
+    }
+}

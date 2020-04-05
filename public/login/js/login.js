d1f35d696b30dc962ac3214b7a9b174bdd11bfb1
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-04-05 23:40:19 +0200

    fix login

--- public/login/js/login.js
+++ public/login/js/login.js
@@ -1,15 +1,15 @@
 /*login script*/
-function loginFct() {
-    var url = 'http://localhost:3000/login/fetch';
-    var resp;
-    
-    fetch(url)
-        .then(function (response) {
-            resp = response.json();
-        });
-    
-    if (response.status == 401) {//resp or response ?
-            var m = document.getElementById("login-error-message");
-            m.innerHTML = "Erreur, mauvais mot de passe ou username.";
-    }
-}
+// function loginFct() {
+//     var url = 'http://localhost:3000/login/fetch';
+//     var resp;
+//
+//     fetch(url)
+//         .then(function (response) {
+//             resp = response.json();
+//         });
+//
+//     if (response.status == 401) {//resp or response ?
+//             var m = document.getElementById("login-error-message");
+//             m.innerHTML = "Erreur, mauvais mot de passe ou username.";
+//     }
+// }

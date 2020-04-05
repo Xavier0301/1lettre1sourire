e5b50df390336cf908a2f061ef54262605613485
BDURON99 <33147418+BDURON99@users.noreply.github.com>
2020-04-05 03:41:59 +0200

    file reorganization

--- public/login/login.js
+++ public/login/login.js
@@ -1,1 +1,11 @@
 /*login script*/
+// DOCTYPE javascript>
+
+function validateLogin() {
+    if (true) {
+        window.location = "index.html";//@TODO remplasser index.html par la page de review
+        return false;
+    } else {
+        alert ("Mauvais nom d'utilisateur ou mot de passe, essaie à nouveau");
+    }
+}

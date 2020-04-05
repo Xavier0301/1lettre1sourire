571917ec1a8651a0336bbe40f7cc43e2cbf9d9ac
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-04-06 00:58:22 +0200

    pusher la salope

--- public/login/js/login.js
+++ public/login/js/login.js
@@ -1,13 +1,13 @@
-//  function funcLogin() {
-//      var url = 'http://localhost:3000/login';
-//      var xd = "xd";
-//
-//      fetch(url)
-//          .then(function (response) {
-//              console.log(response);
-//              console.log(xd);
-//              let resp = response.json();
-//          });
-//
-//
-// };
+ //  function funcLogin() {
+ //      var url = 'http://localhost:3000/login';
+ //      var xd = "xd";
+ //
+ //      fetch(url)
+ //          .then(function (response) {
+ //              console.log(response);
+ //              console.log(xd);
+ //              let resp = response.json();
+ //          });
+ //
+ //
+ // };

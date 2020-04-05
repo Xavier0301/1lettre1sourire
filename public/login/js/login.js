e6025a2fb07fbdf18767634455657d096ef5bf9e
BDURON99 <33147418+BDURON99@users.noreply.github.com>
2020-04-05 14:57:32 +0200

    Update login.js

--- public/login/js/login.js
+++ public/login/js/login.js
@@ -1,11 +1,17 @@
 /*login script*/
 // DOCTYPE javascript>
 
-function validateLogin() {
-    if (true) {
-        window.location = "index.html";//@TODO remplasser index.html par la page de review
-        return false;
-    } else {
-        alert ("Mauvais nom d'utilisateur ou mot de passe, essaie à nouveau");
-    }
-}
+fetch(./app/middleware/loginReq.js)
+    .then(function(response){
+        if (response.status !== 401){
+            //good login go to reviewer window
+            window.location = "reviewer.html";
+            return;
+        } else {
+            var m = document.getElementById("login-error-message");
+            m.innerHTML = "Erreur, mauvais mot de passe ou username.";
+        }
+}).catch(function(err){
+    //if the server returns some errors
+    console.log('Fetch error ', err);
+});

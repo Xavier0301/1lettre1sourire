9cf381edb9049d81ef10bf1112b4f7247b3763d6
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-04-05 23:19:56 +0200

    GET by fetch, display img and letter

--- public/review/js/scriptWrite.js
+++ public/review/js/scriptWrite.js
@@ -1,1 +1,10 @@
-alert("bigoof");
+function displayLetter(letterCore,greetingCore,signatureCore){
+  document.getElementById("letterSpan").innerHTML = letterCore;
+  document.getElementById("greetingSpan").innerHTML = greetingCore;
+  document.getElementById("signatureSpan").innerHTML = signatureCore;
+}
+
+function displayImage(imgsrc){
+  document.getElementById("imageLetter").src = imgsrc;
+
+}

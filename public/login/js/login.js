3d17fcae1477debae32be873bce0a8abb5273f41
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-04-06 03:42:10 +0200

    skrrrr

--- public/login/js/login.js
+++ public/login/js/login.js
@@ -1,11 +1,52 @@
-// function funcLogin() {
-//   var url = 'http://localhost:3000/login';
-//   var xd = "xd";
-//
-//   fetch(url)
-//     .then(function (response) {
-//         console.log(response);
-//         console.log(xd);
-//         let resp = response.json();
-//       });
-//  };
+ function funcLogin() {
+   var url = 'http://localhost:3000/login';
+
+
+//    fetch(url)
+//     .then(function(response) {
+//       if (response.status !== 401) {
+//         console.log("in if");
+//         throw new Error("Not 200 response")
+//     } else {
+//       console.log("in else");
+//       console.log(response);
+//     }
+// }).catch(function(err) {
+//     console.log(err);
+//     console.log("in catch");
+// });
+
+    // fetch(url)
+    //   .then(function(resp){
+    //     if(resp.status !== 401){
+    //       return funcLogin();
+    //     }else{
+    //       console.log(xd);
+    //     }
+    //   });
+
+
+    let data = {
+      password: document.getElementById("password").value,
+      username: document.getElementById("username").value
+    }
+
+    let fetchData = {
+      method: 'POST',
+      body: JSON.stringify(data),
+      headers: new Headers()
+    }
+
+    fetch(url, fetchData)
+      .then(function(response){
+        console.log(fetchData);
+        console.log(response);
+        if(response.status !== 200){
+          console.log("error not 200")
+        } else {
+          console.log("welcome")
+        }
+      });
+
+
+}

bc5b949d71ac4bcc6590eb170b7ef9998d3eb2c8
Servax314 <59235675+Servax314@users.noreply.github.com>
2020-04-06 12:01:08 +0200

    trigger on enter for loggin and admin

--- public/review/js/scriptFetch.js
+++ public/review/js/scriptFetch.js
@@ -1,21 +1,27 @@
 
 //var content; //[id,type,greeting,content,signature,imageUrl,exists]
 var idToPost;
+var localCounter = 0;
 function funcFetch() {
   var url = 'http://localhost:3000/review/fetch';
 
   fetch(url)
     .then((response) => response.json())
     .then(function(data) {
-      let contentLetter = data.content;
-      let contentImage = data.imageUrl;
-      let greetingLetter = data.greeting;
-      let signatureLetter = data.signature;
-      idToPost = data.id;
-      console.log(contentLetter);
-      console.log(contentImage);
-      console.log(data);
-      displayLetter(contentLetter,greetingLetter,signatureLetter);
-      displayImage(contentImage);
+      if(data.exists) {
+        localCounter += 1;
+        let contentLetter = data.content;
+        let contentImage = data.imageUrl;
+        let greetingLetter = data.greeting;
+        let signatureLetter = data.signature;
+        idToPost = data.id;
+        console.log(contentLetter);
+        console.log(contentImage);
+        console.log(data);
+        displayLetter(contentLetter,greetingLetter,signatureLetter);
+        displayImage(contentImage);
+      } else {
+        displayLetter(" il n'y a actuellement plus de lettre à passer en revue.", "Hey,", "Nous te remercions pour votre collaboration de " + localCounter + " lettres !")
+      }
     });
 };

7d34b027c0934b034b4fcc51e6a7d05578ca6b2a
Xavier Servot <xavier.servot@live.fr>
2020-04-08 06:05:46 +0200

    remove db config

--- config/db.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-   url: 'mongodb://localhost:27017/test',
-   options: {
-      user: "Xavier0301",
-      pass: "ZSEY0jFjNQpuI7hPnx",
-      useMongoClient: true,
-      authSource: "admin"
-   }
-}
